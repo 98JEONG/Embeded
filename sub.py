8b77ce9f11a5a21857ba86a84f027ab11b727bbe
leehyojeong <39904216+leehyojeong@users.noreply.github.com>
2019-12-10 17:29:48 +0900

    Update sub.py

--- sub.py
+++ sub.py
@@ -8,9 +8,12 @@ Created on Tue Dec  3 23:44:34 2019
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
+    print("pub-sub 연결")
     client.subscribe("embedded/mqtt/project")
     
 def on_message(client,userdata,msg):
+    print(str(msg.payload.decode("utf-8")))
+    
     if str(msg.payload.decode("utf-8"))!="PLAY":
         #play가 아니면 chatting내용임
         print(str(msg.payload.decode("utf-8")))
@@ -25,4 +28,3 @@ if __name__=='__main__':
             client.loop_forever()
     except KeyboardInterrupt:
         exit()
-        
